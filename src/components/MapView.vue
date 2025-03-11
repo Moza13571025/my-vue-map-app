e2eaef851689051b56ee43e0bd78383089c41077
Moza <moza13571025@gmail.com>
2025-03-11 16:36:35 +0800

    建立第一個名為MapView，使用Leaflet顯示地圖的vue組件

---
+++ src/components/MapView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen">
+    <div id="map" class="w-full h-full"></div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import L from "leaflet";
+
+export default {
+  setup(){
+    onMounted(()=>{
+      const map = L.map("map").setView([25.033, 121.5654], 13)//預設地圖中心和縮放級別
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors",
+      }).addTo(map);//引入 OpenStreetMap 圖磚服務 API並加入版權資訊、放入map中。
+    })
+  }
+}
+</script>
+
+<style>
+#map {
+  height: 100vh;
+}
+</style>
