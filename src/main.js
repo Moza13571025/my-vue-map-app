4f72972dcb44372238307d65b4d24647c1d34e7a
Moza <moza13571025@gmail.com>
2025-03-11 17:09:02 +0800

    建立 Vue Router，載入地圖頁面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "vuetify/styles";
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import router from "./router";
 
 const vuetify = createVuetify();
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
---
+++ src/router.js
@@ -0,0 +1,15 @@
+//1.引入必要模組
+import { createRouter, createWebHistory } from "vue-router"; //用vue-router的內建函式，建立路由實例、設定路由模式。
+import MapView from "./components/MapView.vue"; //在特定url下渲染指定的vue元件
+
+//2.定義路由
+const routes = [{ path: "/", components: MapView }];
+
+//3.以createRouter()建立路由實例(instance)
+const router = createRouter({
+  history: createWebHistory(), //使用 HTML5 的 History API，使 URL 看起來更乾淨
+  routes,
+});
+
+//4.匯出路由實例
+export default router;
